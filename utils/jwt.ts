d82406402e2c9e7f36e24d070af645398d9bda2e
Miguel Rafael <contato.Miguel.almeida7@gmail.com>
2024-07-19 10:25:42 -0300

    Update jwt.ts

--- utils/jwt.ts
+++ utils/jwt.ts
@@ -43,9 +43,9 @@ export const sendToken = (user: IUser, statusCode: number, res: Response) => {
 
   redis.set(user._id, JSON.stringify(user) as any);
 
-  // if (process.env.NODE_ENV === "production") {
-  //   accessTokenOptions.secure = true;
-  // }
+  if (process.env.NODE_ENV === "production") {
+    accessTokenOptions.secure = true;
+  }
 
   res.cookie("access_token", accessToken, accessTokenOptions);
   res.cookie("refresh_token", refreshToken, refreshTokenOptions);
